b82de524e4961162994e52e7ed07bbb7f3db480e
Andreas Stöckel <astoecke@uwaterloo.ca>
2019-05-24 11:07:07 -0400

    Fix parisien example

--- examples/parisien.py
+++ examples/parisien.py
@@ -10,20 +10,27 @@ with nengo.Network() as model:
     # Excitatory source population
     ens_source = bio.Ensemble(n_neurons=101, dimensions=1, p_exc=1.0)
 
-    # Inhibitory interneuron population
+    # Inhibitory inter-neuron population
     ens_inhint = bio.Ensemble(n_neurons=102, dimensions=1, p_inh=1.0)
+                              #intercepts=nengo.dists.Uniform(-1.5, -0.25))
 
     # Target population
     ens_target = bio.Ensemble(n_neurons=103, dimensions=1)
 
     nengo.Connection(inp_a, ens_source)
-    bio.Connection(ens_source, ens_inhint, label="s->i", decode_bias=True)
+    bio.Connection(ens_source, ens_inhint)
     bio.Connection((ens_source, ens_inhint), ens_target,
-                   function=lambda x: np.mean(x), label="s,i->t")
+                   function=lambda x: np.mean(x)**2,
+                   eval_points=[1, 1] * np.linspace(-1, 1, 1000)[:, None])
 
-     # Note: the solver of the last connection has access to the state of both
-     # the pre-populations at the same time. This is equivalent to a connection
-     # from a 2D ensemble with orthogonal encoders.
+#    # TODO: The above should be simpler, probably allow something such as
+#    bio.Connection(Join(ens_source, ens_inhint), ens_target,
+#                   function=lambda x: x**2)
+#    # Where the default operation is "Stack"
+
+#    # Note: the solver of the last connection has access to the state of both
+#    # the pre-populations at the same time. This is equivalent to a connection
+#    # from a 2D ensemble with orthogonal encoders.
 #    ens_combined = nengo.Ensemble(n_neurons=203, dimensions=2,
 #                    encoders=nengo.dists.Choice([[0, 1], [1, 0], [0, -1], [-1, 0]]))
 #    nengo.Connection(inp_a, ens_combined[0])
