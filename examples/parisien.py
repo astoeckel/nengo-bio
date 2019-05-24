113f3bcf3bfe8f35e5b4ce656fa07f1bc33eedf9
Andreas Stöckel <astoecke@uwaterloo.ca>
2019-05-23 22:47:33 -0400

    add Parisien transform example

---
+++ examples/parisien.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import nengo
+import nengo_bio as bio
+import numpy as np
+
+with nengo.Network() as model:
+    inp_a = nengo.Node(0)
+
+    # Excitatory source population
+    ens_source = bio.Ensemble(n_neurons=101, dimensions=1, p_exc=1.0)
+
+    # Inhibitory interneuron population
+    ens_inhint = bio.Ensemble(n_neurons=102, dimensions=1, p_inh=1.0)
+
+    # Target population
+    ens_target = bio.Ensemble(n_neurons=103, dimensions=1)
+
+    nengo.Connection(inp_a, ens_source)
+    bio.Connection(ens_source, ens_inhint, label="s->i", decode_bias=True)
+    bio.Connection((ens_source, ens_inhint), ens_target,
+                   function=lambda x: np.mean(x), label="s,i->t")
+
+     # Note: the solver of the last connection has access to the state of both
+     # the pre-populations at the same time. This is equivalent to a connection
+     # from a 2D ensemble with orthogonal encoders.
+#    ens_combined = nengo.Ensemble(n_neurons=203, dimensions=2,
+#                    encoders=nengo.dists.Choice([[0, 1], [1, 0], [0, -1], [-1, 0]]))
+#    nengo.Connection(inp_a, ens_combined[0])
+#    nengo.Connection(ens_combined[0], ens_combined[1])
+#    nengo.Connection(ens_combined, ens_target, function=lambda x: np.mean(x))
+
+with nengo.Simulator(model, progress_bar=None) as sim:
+    sim.run(1.0)
