5cca86f997cce96ad9aa438ac7f6805d62587506
Andreas Stöckel <astoecke@uwaterloo.ca>
2019-06-10 17:01:23 -0400

    Use set_defaults in example

--- examples/communication_channel_two_comp_lif.py
+++ examples/communication_channel_two_comp_lif.py
@@ -4,27 +4,24 @@ import nengo
 import nengo_bio as bio
 import numpy as np
 
+bio.set_defaults()
+
 with nengo.Network(seed=4589) as model:
     inp_a = nengo.Node(0)
     inp_b = nengo.Node(0)
 
-    ens_a = bio.Ensemble(n_neurons=101, dimensions=1, p_exc=0.5,
-                         max_rates=nengo.dists.Uniform(50, 100),
-                         intercepts=nengo.dists.Uniform(-0.95, 0.95))
+    ens_a = bio.Ensemble(n_neurons=101, dimensions=1, p_exc=0.5)
 
-    ens_b = bio.Ensemble(n_neurons=102, dimensions=1, p_exc=0.5,
-                         max_rates=nengo.dists.Uniform(50, 100),
-                         intercepts=nengo.dists.Uniform(-0.95, 0.95))
+    ens_b = bio.Ensemble(n_neurons=102, dimensions=1, p_exc=0.5)
 
-    ens_c = bio.Ensemble(n_neurons=103, dimensions=2,
-                         neuron_type=bio.neurons.TwoCompLIF(),
-                         intercepts=nengo.dists.Uniform(-0.95, 0.95),
-                         max_rates=nengo.dists.Uniform(50, 100))
+    ens_c = bio.Ensemble(n_neurons=103, dimensions=2, radius=np.sqrt(2),
+                         neuron_type=bio.neurons.TwoCompLIF())
 
     nengo.Connection(inp_a, ens_a)
     nengo.Connection(inp_b, ens_b)
 
-    bio.Connection((ens_a, ens_b), ens_c)
+    bio.Connection((ens_a, ens_b), ens_c,
+                   solver=bio.solvers.QPSolver(relax=True))
 
 with nengo.Simulator(model, progress_bar=None, dt=1e-4) as sim:
     sim.run(1.0)
